7c3e2f2ca4c4d742208bb7d40eaa51cd149c248c
kroopasreesai <kroopasreesai@gmail.com>
2024-02-28 10:57:30 +0530

    second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "Hello, roopa\n";
+  cout << "Updated successfully\n";
   return 0;
 }
